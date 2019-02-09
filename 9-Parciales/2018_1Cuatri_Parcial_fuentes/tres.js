a335e6737d1197c271a48e8036fc48806d83824a
RojasFranco <rojas.franco.93@gmail.com>
2019-02-08 21:48:02 -0300

    ej 1-2-3 del parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,10 @@
 function mostrar()
 {
-
+    var precio;
+    var descuento;
+    var precioFinal;
+    precio=prompt("Ingrese precio");
+    descuento=prompt("Ingrese % de descuento");
+    precioFinal=parseInt(precio)*(100-parseInt(descuento))/100;
+    document.getElementById("elPrecioFinal").value=precioFinal;
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,10 @@
 
 function mostrar()
 {
-
+    var ancho;
+    var largo;
+    ancho=prompt("cuanto mide el ancho del rectangulo?");
+    largo=prompt("cuando mide el largo del rectangulo");
+    perimetro=parseInt(ancho)*2 + parseInt(largo)*2;
+    alert('el perimetro del rectangulo es '+ perimetro)
 }
