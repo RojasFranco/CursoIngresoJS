15ab80eea63406fe0cf134433c73f37e3c801a68
RojasFranco <rojas.franco.93@gmail.com>
2019-02-08 22:16:46 -0300

    recuperatorio y 1er ej tp

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,28 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+var precio1,precio2,precio3;
 function Sumar () 
 {
-	
+    precio1=document.getElementById("PrecioUno").value;
+    precio2=document.getElementById("PrecioDos").value;
+    precio3=document.getElementById("PrecioTres").value;
+    suma=parseInt(precio1)+parseInt(precio2)+parseInt(precio3);
+    alert("la suma es: "+suma);
 }
 function Promedio () 
 {
-	
+    precio1=document.getElementById("PrecioUno").value;
+    precio2=document.getElementById("PrecioDos").value;
+    precio3=document.getElementById("PrecioTres").value;
+    suma=parseInt(precio1)+parseInt(precio2)+parseInt(precio3);
+    alert("el promedio es: "+suma/3);
 }
 function PrecioFinal () 
 {
-	
+	precio1=document.getElementById("PrecioUno").value;
+    precio2=document.getElementById("PrecioDos").value;
+    precio3=document.getElementById("PrecioTres").value;
+    suma=parseInt(precio1)+parseInt(precio2)+parseInt(precio3);
+    alert("el precio final es: "+suma*121/100)
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,10 @@
 
 function mostrar()
 {
-
+    var base, altura, superficie, perimetro;
+    base=prompt("Ingrese base del triangulo")
+    altura=prompt("Ingrese altura del triangulo")
+    superficie=parseInt(base)*parseInt(altura)/2
+    perimetro=base*3
+    alert("La superficie es: "+superficie+" y el perimetro es : "+perimetro)
 }
