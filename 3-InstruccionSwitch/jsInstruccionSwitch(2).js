65990eb45bb1d1fda165d101fba30f5d286244f6
alumno <alumno>
2019-02-20 19:35:40 -0300

    Ejercicios Switch
    Signed-off-by: alumno <alumno>

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,7 +2,23 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
+    switch(mesDelAño) {
+        case 'Julio':/*no hace falta agregar nada ya que al no haber un break sigue con agosto*/
+        case 'Agosto':
+            alert('Abrigate que hace frio');
+            break;
+        case 'Enero':
+        case 'Febrero':
+        case 'Marzo':
+        case 'Abril':
+        case 'Mayo':
+        case 'Junio':
+            alert('Falta para el invierno');
+            break;
+        default:
+            alert('Ya pasamos el frio, ahora calor!!!');
+        /*hubiese quedado mejor poniendo los casos donde falta para el invierno en default*/
+    }
 alert (mesDelAño);
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -2,7 +2,22 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
+    switch(mesDelAño) {
+        case 'Febrero':
+            alert('tiene 28 días');
+            break;
+        case 'Enero':
+        case 'Marzo':
+        case 'Mayo':
+        case 'Julio':
+        case 'Agosto':
+        case 'Octubre':
+        case 'Diciembre':
+            alert('Tiene 31 días');
+            break;
+        default:
+            alert('Tiene 30 días');
+    }
 //alert (mesDelAño);
 	
 	
